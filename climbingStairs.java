cee840318d34449f76cc5b95a19bccb77604d17b
Tang Lu <tanglu2004@gmail.com>
2014-02-03 17:52:02 +0900

    Update climbingStairs.java

--- climbingStairs.java
+++ climbingStairs.java
@@ -5,12 +5,12 @@ public class Solution {
             return n;
         }
         return climbStairs(n-1) + climbStairs(n - 2);*/
-        int pre = 1;
-        int result = 2;
         if(n<=2) {
             return n;
-        } 
-        
+        }
+        int pre = 1;
+        int result = 2;
+         
         for(int i=3;i<=n;i++) {
             result = pre + result;
             pre = result - pre;
