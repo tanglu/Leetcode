8314d68ec95718850f74acfef33ee800bc7e33cf
Tang Lu <tanglu2004@gmail.com>
2014-02-10 22:15:47 +0900

    Create pascalTriangleII.java

---
+++ pascalTriangleII.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public ArrayList<Integer> getRow(int rowIndex) {
+        if(rowIndex<0) {
+            return null;
+        }
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        for(int i=0;i<=rowIndex;i++) {
+            array.add(1);
+            for(int j=i-1;j>0;j--) {
+                int tmp = array.get(j) + array.get(j-1);
+                array.set(j,tmp);
+            }
+        }
+        return array;
+    }
+}
